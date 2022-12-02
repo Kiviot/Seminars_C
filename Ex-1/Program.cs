9292148b579f435e638418592d92593676d6c82a
Teppe <me@teppe.ru>
2022-12-02 13:51:51 +0300

    add compare two numbers

--- Ex-1/Program.cs
+++ Ex-1/Program.cs
@@ -1,6 +1,19 @@
-﻿Console.Write("Write number: ");
-int number = Convert.ToInt32(Console.ReadLine()); //тип данных имя = значение
-//int result = number * number; //результат возведения в квадрат
-double result = Math.Pow(number, 2);// вариант через функцию: результат возведения в квадрат
-Console.WriteLine(number); //вывод числа
-System.Console.WriteLine(result);//вывод результата
+﻿// Console.Write("Write number: ");
+// int number = Convert.ToInt32(Console.ReadLine()); //тип данных имя = значение
+// //int result = number * number; //результат возведения в квадрат
+// double result = Math.Pow(number, 2);// вариант через функцию: результат возведения в квадрат
+// Console.WriteLine(number); //вывод числа
+// System.Console.WriteLine(result);//вывод результата
+
+// Console.Write("Write first number: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Write second number: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (secondNumber * secondNumber == firstNumber)
+// {
+//     System.Console.WriteLine("TRUE");
+// }
+// else System.Console.WriteLine("Folse");
+
