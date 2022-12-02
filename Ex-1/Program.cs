9f460b37c664e6dca49929fc1198c6405da65f35
Teppe <me@teppe.ru>
2022-12-02 13:17:52 +0300

    result = number^2

--- Ex-1/Program.cs
+++ Ex-1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 5; //тип данных имя = значение
+int result = number * number; //результат возведения в квадрат
+Console.WriteLine(number); //вывод числа
+System.Console.WriteLine(result);//вывод результата
