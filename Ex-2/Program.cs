95cb7cf4df9b18e62440522b8dcf0950b652b614
Teppe <me@teppe.ru>
2022-12-15 12:11:56 +0300

    commit write the matrix

--- Ex-2/Program.cs
+++ Ex-2/Program.cs
@@ -221,3 +221,60 @@
 // PrintArray(array);
 
 // ==========================================================================================
+// ==========================================================================================
+// Лекция №4 
+// Двумерные массивы и рекурсия
+
+string[,] table = new string[2, 5];
+//String.Empty - инициализация массива
+// table[0, 0] table[0, 1] table[0, 2] table[0, 3] table[0, 4]
+// table[1, 0] table[1, 1] table[1, 2] table[1, 3] table[1, 4]
+
+// ==========================================================================================
+
+// table[1, 2] = "word";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         System.Console.WriteLine($"-{table[rows, colums]}-");
+//     }
+// }
+// ==========================================================================================
+// int[,] matrix = new int[3, 4];
+// for (int rows = 0; rows < matrix.GetLength(0); rows++)
+// {
+//     for (int colums = 0; colums < matrix.GetLength(1); colums++)
+//     {
+//         System.Console.Write($"{matrix[rows, colums]} ");
+//     }
+//     System.Console.WriteLine();
+// }
+// ==========================================================================================
+
+// void PrintArray(int[,] matr, int minValue, int maxValue)
+// {
+//     int sizeRows = matr.GetLength(0);
+//     int sizeColums = matr.GetLength(1);
+//     for (int i = 0; i < sizeRows; i++)
+//     {
+//         for (int j = 0; j < sizeColums; j++)
+//         {
+//             matr[i, j] = new Random().Next(minValue, maxValue);
+//             System.Console.Write($"{matr[i, j]} ");
+//         }
+//         System.Console.WriteLine();
+// }
+// }
+
+// Console.WriteLine("Введите нижнюю границу диапазона: ");
+// int minValue = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите верхнюю границу диапазона: ");
+// int maxValue = int.Parse(Console.ReadLine()!);
+
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix, minValue, maxValue);
+
+// ==========================================================================================
+
