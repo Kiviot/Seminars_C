bcb3bde40bbeb2e29f30cabc5e33a07cdb68754d
Teppe <me@teppe.ru>
2022-12-20 11:30:58 +0300

    commit HomeWork Ex.42

--- Seminars/Program.cs
+++ Seminars/Program.cs
@@ -1687,33 +1687,67 @@
 -1, -7, 567, 89, 223-> 3
 */
 
-int[] GetArray(int len)
-{
-    int[] array = new int[len];
-    int size = array.Length;
-    for (int i = 0; i < size; i++)
-    {
-        System.Console.WriteLine("введите произольные ЦЕЛЫЕ числа:");
-        array[i] = int.Parse(Console.ReadLine()!);
-    }
-    return array;
-}
+// int[] GetArray(int len)
+// {
+//     int[] array = new int[len];
+//     int size = array.Length;
+//     for (int i = 0; i < size; i++)
+//     {
+//         System.Console.WriteLine("введите произольные ЦЕЛЫЕ числа:");
+//         array[i] = int.Parse(Console.ReadLine()!);
+//     }
+//     return array;
+// }
 
-int CountNumber(int[] array)
-{
-int countNumber = 0;
-int size = array.Length;
-for (int i = 0; i < size; i++)
-    {
-        countNumber += array[i] > 0 ? 1 : 0;
-    }
-    return countNumber;
-}
-Console.WriteLine("Введите сколько Вы хотите ввести чисел: ");
-int len = int.Parse(Console.ReadLine()!);
-int[] array = GetArray(len);
-System.Console.WriteLine($"[{string.Join(",", array)}]");
-int CountItems = CountNumber(array);
-System.Console.WriteLine(CountItems);
+// int CountNumber(int[] array)
+// {
+// int countNumber = 0;
+// int size = array.Length;
+// for (int i = 0; i < size; i++)
+//     {
+//         countNumber += array[i] > 0 ? 1 : 0;
+//     }
+//     return countNumber;
+// }
+// Console.WriteLine("Введите сколько Вы хотите ввести чисел: ");
+// int len = int.Parse(Console.ReadLine()!);
+// int[] array = GetArray(len);
+// System.Console.WriteLine($"[{string.Join(",", array)}]");
+// int CountItems = CountNumber(array);
+// System.Console.WriteLine(CountItems);
+
+// ==========================================================================================
+/*
+Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+*/
+
+System.Console.WriteLine("введите значение b1:");
+double b1 = Convert.ToInt64(Console.ReadLine()!);
+System.Console.WriteLine("введите коэффициент k1:");
+double k1 = Convert.ToInt64(Console.ReadLine()!);
+
+System.Console.WriteLine("введите значение b2:");
+double b2 = Convert.ToInt64(Console.ReadLine()!);
+System.Console.WriteLine("введите коэффициент k2:");
+double k2 = Convert.ToInt64(Console.ReadLine()!);
+
+// double b1 = 2;
+// double k1 = 5;
+// double b2 = 4;
+// double k2 = 9;
+
+double x = ((b2 - b1) / (k1 - k2));
+System.Console.WriteLine("x = " + x);
+double y1 = k1 * x + b1;
+System.Console.WriteLine("y1 = " + y1);
+double y2 = k2 * x + b2;
+System.Console.WriteLine("y2 = " + y2);
+
+System.Console.WriteLine($"точка пересечений двух прямых равна ({x}, {y1})");
 
 // ==========================================================================================
