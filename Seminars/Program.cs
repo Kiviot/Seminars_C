ef1275ab1b29b3c24896daa9e49d8ebe708a0ca9
Teppe <me@teppe.ru>
2022-12-30 11:23:59 +0300

    commit Last HomeWork for C#

--- Seminars/Program.cs
+++ Seminars/Program.cs
@@ -3129,4 +3129,95 @@ A = 2; B = 3 -> 8
 // }
 // System.Console.WriteLine(PowNumber (firstNumber, secondNumber));
 
+///HomeWork
+
+/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 6, 7, 8"
+*/
+
+//Задача 64
+// System.Console.WriteLine("Write firstNumber: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Write secondNumber: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// string IntervalNumber (int firstDigit, int secondDigit)
+// {
+//         if(firstDigit > secondDigit)
+//         {
+//                 int temp = firstDigit;
+//                 firstDigit = secondDigit;
+//                 secondDigit = temp;
+//         }
+//         if (firstDigit == secondDigit) return firstDigit.ToString();
+//         return Convert.ToString(firstDigit + ", " + IntervalNumber(firstDigit + 1, secondDigit));
+// }
+
+// System.Console.WriteLine(IntervalNumber (firstNumber, secondNumber));
+
+
+/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+// Задача 66
+
+// System.Console.WriteLine("Write firstNumber: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Write secondNumber: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// int SumIntervalDigits(int startDigit, int lastDigit)
+// {
+//         if (startDigit > lastDigit)
+//         {
+//                 int temp = startDigit;
+//                 startDigit = lastDigit;
+//                 lastDigit= temp;
+//         }
+//         if (startDigit == lastDigit) return startDigit;
+//         return startDigit + SumIntervalDigits(startDigit + 1, lastDigit);
+// }
+// System.Console.WriteLine($"Сумма натуральных элементов между числами {firstNumber} и {secondNumber} равна: {SumIntervalDigits(firstNumber, secondNumber)}");
+
+
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+m = 2, n = 3 -> A(m,n) = 29
+*/
+
+// Задача 68
+
+// System.Console.WriteLine("Write firstNumber: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Write secondNumber: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if(firstNumber < 0 || secondNumber < 0) 
+// {
+//         System.Console.WriteLine("Введено отрицательное значение");
+//         return;
+// }
+
+// double AkkermanFunnction(double m, double n)
+// {
+//         if (m == 0) return n + 1;
+//         else
+//         { 
+//                 if (m != 0 && n == 0) return AkkermanFunnction (m-1, 1);
+//                 else return AkkermanFunnction (m-1, AkkermanFunnction (m, n - 1));
+//         }
+// }
+
+// double A = AkkermanFunnction(firstNumber, secondNumber);
+// System.Console.WriteLine($"Значение функции аккермана: Akkerman ({firstNumber}, {secondNumber}) = {A}");
+
 ///
+
